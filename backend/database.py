b401f7925512b0e662043c19c7e2f4e6f4fff58b
AMAN <151147203+AmanTShekar@users.noreply.github.com>
2025-09-09 16:49:48 +0530

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -1,11 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./ecommerce.db"
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./ecommerce.db")
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False} if "sqlite" in DATABASE_URL else {})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
